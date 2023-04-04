b8402e00e6811463c6fd04f54f2b763ddf629d1d
xicasal <xicasal17@esei.uvigo.es>
2023-04-04 11:37:38 +0200

    Añadido sql de experiencia_viajes

--- sql_definitivo_experiencia_viajes/renfe.sql
+++ sql_definitivo_experiencia_viajes/renfe.sql
@@ -37,7 +37,9 @@ CREATE TABLE `dim_billete` (
   `PLAZA` varchar(3) NOT NULL,
   `CLASE` varchar(15) NOT NULL,
   `MASCOTA` tinyint(1) NOT NULL,
-  `BONO` varchar(15) DEFAULT NULL
+  `BONO_JOVEN` tinyint(1) DEFAULT NULL,
+  `BONO_NUMEROSA` tinyint(1) DEFAULT NULL,
+  `BONO_JUBILADO` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -62,6 +64,7 @@ CREATE TABLE `dim_date` (
 
 CREATE TABLE `dim_estacion` (
   `ESTACION_KEY` int(11) NOT NULL,
+  `ID_ESTACION` bigint(20) NOT NULL,
   `ESTACION_ULTIMA_ACTUALIZACION` datetime NOT NULL,
   `ESTACION_VERSION` varchar(20) NOT NULL,
   `VALIDO_DESDE` datetime NOT NULL,
@@ -105,23 +108,6 @@ CREATE TABLE `dim_time` (
   `AM_PM` char(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `dim_trayecto`
---
-
-CREATE TABLE `dim_lugar` (
-  `LUGAR_KEY` int(11) NOT NULL,
-  `ID_LUGAR` bigint(20) NOT NULL,
-  `VALIDO_DESDE` datetime NOT NULL,
-  `VALIDO_HASTA` datetime NOT NULL,
-  `VERSION` varchar(20) NOT NULL,
-  `NOMBRE` varchar(25) NOT NULL,
-  `CIUDAD` varchar(20) NOT NULL,
-  `TIPO_CIUDAD` varchar(15) NOT NULL
-  
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -132,21 +118,16 @@ CREATE TABLE `dim_lugar` (
 CREATE TABLE `dim_viaje` (
   `VIAJE_KEY` int(11) NOT NULL,
   `ID_VIAJE` bigint(20) NOT NULL,
-  `ID_ESTACION` decimal(20,0) NOT NULL,
   `VIAJE_ULTIMA_ACTUALIZACION` datetime NOT NULL,
-  `KILOMETROS_TOTALES` bigint(20) NOT NULL,
-  `NUM_PARADAS` smallint(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `dim_viaje_trayecto_bridge`
---
-
-CREATE TABLE `dim_viaje_estacion_bridge` (
-  `VIAJE_KEY` int(11) NOT NULL,
-  `ESTACION_KEY` int(11) NOT NULL
+  `VALIDO_DESDE` datetime NOT NULL,
+  `VALIDO_HASTA` datetime NOT NULL,
+  `VERSION` varchar(20) NOT NULL,
+  `C_ORIGEN` varchar(20) NOT NULL,
+  `C_DESTINO` varchar(20) NOT NULL,
+  `FECHA_LLEGADA` datetime NOT NULL,
+  `FECHA_SALIDA` datetime NOT NULL,
+  `NUM_BILLETES` smallint NOT NULL,
+  `KILOMETROS_TOTALES` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -155,19 +136,25 @@ CREATE TABLE `dim_viaje_estacion_bridge` (
 -- Estructura de tabla para la tabla `fact_venta`
 --
 
-CREATE TABLE `fact_venta` (
-  `ID_VENTA` bigint(20) NOT NULL,
+CREATE TABLE `fact_experiencia_viaje` (
+  `ID_EXPERIENCIA_VIAJE` bigint(20) NOT NULL,
   `VIAJE_KEY` int(11) NOT NULL,
   `PASAJERO_KEY` int(11) NOT NULL,
-  `LUGAR_KEY` int(11) NOT NULL,
-  `DATE_KEY` int(11) NOT NULL,
-  `TIME_KEY` int(11) NOT NULL,
+  `ESTACION_ORIGEN_KEY` int(11) NOT NULL,
+  `ESTACION_DESTINO_KEY` int(11) NOT NULL,
+  `DATE_ORIGEN_KEY` int(11) NOT NULL,
+  `DATE_DESTINO_KEY` int(11) NOT NULL,
+  `TIME_ORIGEN_KEY` int(11) NOT NULL,
+  `TIME_DESTINO_KEY` int(11) NOT NULL,
   `BILLETE_KEY` int(11) NOT NULL,
   `VENTA_ULTIMA_ACTUALIZACION` datetime NOT NULL,
   `UNIDADES` int(11) NOT NULL,
   `PRECIO_FINAL` float NOT NULL,
-  `SALIDA` datetime NOT NULL,
-  `LLEGADA` datetime NOT NULL
+  `REPITE_VIAJE` BOOLEAN NOT NULL,
+  `VALORACION_POSITIVA` BOOLEAN NOT NULL,
+  `VALORACION_NEGATIVA` BOOLEAN NOT NULL,
+  `VALORACION_REGULAR` BOOLEAN NOT NULL,
+  `DIFERENCIA` varchar(20) NOT NULL -- Diferencia entre hora teorica y la real en realizar el viaje
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -204,55 +191,44 @@ ALTER TABLE `dim_pasajero`
 ALTER TABLE `dim_time`
   ADD PRIMARY KEY (`TIME_KEY`);
 
---
--- Indices de la tabla `dim_trayecto`
---
-ALTER TABLE `dim_lugar`
-  ADD PRIMARY KEY (`LUGAR_KEY`);
 
---
 -- Indices de la tabla `dim_viaje`
 --
 ALTER TABLE `dim_viaje`
   ADD PRIMARY KEY (`VIAJE_KEY`);
 
 --
--- Indices de la tabla `dim_viaje_trayecto_bridge`
 --
-ALTER TABLE `dim_viaje_estacion_bridge`
-  ADD PRIMARY KEY (`VIAJE_KEY`,`ESTACION_KEY`),
-  ADD KEY `FK_ESTACION_KEY` (`ESTACION_KEY`);
-
+-- Indices de la tabla `fact_experiencia_viaje`
 --
--- Indices de la tabla `fact_venta`
---
-ALTER TABLE `fact_venta`
+ALTER TABLE `fact_experiencia_viaje`
   ADD KEY `FK_DIM_VIAJE` (`VIAJE_KEY`),
   ADD KEY `FK_DIM_PASAJERO` (`PASAJERO_KEY`),
-  ADD KEY `FK_DIM_LUGAR` (`LUGAR_KEY`),
-  ADD KEY `FK_DIM_DATE` (`DATE_KEY`),
-  ADD KEY `FK_DIM_TIME` (`TIME_KEY`);
+  ADD KEY `FK_DIM_ESTACION_ORIGEN` (`ESTACION_ORIGEN_KEY`),
+  ADD KEY `FK_DIM_ESTACION_DESTINO` (`ESTACION_DESTINO_KEY`),
+  ADD KEY `FK_DIM_DATE_ORIGEN` (`DATE_ORIGEN_KEY`),
+  ADD KEY `FK_DIM_DATE_DESTINO` (`DATE_DESTINO_KEY`),
+  ADD KEY `FK_DIM_TIME_ORIGEN` (`TIME_ORIGEN_KEY`),
+  ADD KEY `FK_DIM_TIME_DESTINO` (`TIME_DESTINO_KEY`),
+  ADD KEY `FK_DIM_BILLETE` (`BILLETE_KEY`);
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `dim_viaje_trayecto_bridge`
+-- Filtros para la tabla `fact_experiencia_viaje`
 --
-ALTER TABLE `dim_viaje_estacion_bridge`
-  ADD CONSTRAINT `FK_ESTACION_KEY` FOREIGN KEY (`ESTACION_KEY`) REFERENCES `dim_estacion` (`ESTACION_KEY`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_VIAJE_KEY` FOREIGN KEY (`VIAJE_KEY`) REFERENCES `dim_viaje` (`VIAJE_KEY`) ON DELETE CASCADE;
-
---
--- Filtros para la tabla `fact_venta`
---
-ALTER TABLE `fact_venta`
-  ADD CONSTRAINT `FK_DIM_DATE` FOREIGN KEY (`DATE_KEY`) REFERENCES `dim_date` (`DATE_KEY`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_DIM_LUGAR` FOREIGN KEY (`LUGAR_KEY`) REFERENCES `dim_lugar` (`LUGAR_KEY`) ON DELETE CASCADE,
+ALTER TABLE `fact_experiencia_viaje`
+  ADD CONSTRAINT `FK_DIM_DATE_ORIGEN` FOREIGN KEY (`DATE_ORIGEN_KEY`) REFERENCES `dim_date` (`DATE_KEY`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_DIM_DATE_DESTINO` FOREIGN KEY (`DATE_DESTINO_KEY`) REFERENCES `dim_date` (`DATE_KEY`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_DIM_TIME_ORIGEN` FOREIGN KEY (`TIME_ORIGEN_KEY`) REFERENCES `dim_time` (`TIME_KEY`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_DIM_TIME_DESTINO` FOREIGN KEY (`TIME_DESTINO_KEY`) REFERENCES `dim_time` (`TIME_KEY`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_DIM_ESTACION_ORIGEN` FOREIGN KEY (`ESTACION_ORIGEN_KEY`) REFERENCES `dim_estacion` (`ESTACION_KEY`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_DIM_ESTACION_DESTINO` FOREIGN KEY (`ESTACION_DESTINO_KEY`) REFERENCES `dim_estacion` (`ESTACION_KEY`) ON DELETE CASCADE,
   ADD CONSTRAINT `FK_DIM_PASAJERO` FOREIGN KEY (`PASAJERO_KEY`) REFERENCES `dim_pasajero` (`PASAJERO_KEY`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_DIM_TIME` FOREIGN KEY (`TIME_KEY`) REFERENCES `dim_time` (`TIME_KEY`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_DIM_VIAJE` FOREIGN KEY (`VIAJE_KEY`) REFERENCES `dim_viaje` (`VIAJE_KEY`) ON DELETE CASCADE;
+  ADD CONSTRAINT `FK_DIM_VIAJE` FOREIGN KEY (`VIAJE_KEY`) REFERENCES `dim_viaje` (`VIAJE_KEY`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_DIM_BILLETE` FOREIGN KEY (`BILLETE_KEY`) REFERENCES `dim_billete` (`BILLETE_KEY`) ON DELETE CASCADE; 
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
